492c5707f5b5fe6c9e7af55244bfe1a841d2f06a
run <maibairun@163.com>
2017-07-26 14:06:54 +0800

    榜单管理添加 日榜、周榜

--- src/components/main/RankPrize.vue
+++ src/components/main/RankPrize.vue
@@ -8,7 +8,13 @@
              :cols="cols"
              :filters="filters"
              :pageSize="pageSize"
+             ref="view"
              :actions="actions">
+    <ul slot="menu" class="duration-tabs">
+      <li><a :class="{active: $route.query.duration==='DATE'}" @click="$refs.view.$refs.table.doQuery({ duration: 'DATE' })">日榜单</a> |</li>
+      <li><a :class="{active: $route.query.duration==='WEEK'}" @click="$refs.view.$refs.table.doQuery({ duration: 'WEEK' })">周榜单</a> |</li>
+      <li><a :class="{active: $route.query.duration==='TOTAL'}" @click="$refs.view.$refs.table.doQuery({ duration: 'TOTAL' })">总榜</a></li>
+    </ul>
   </list-view>
 
 </template>
@@ -17,28 +23,29 @@
   export default {
     data() {
       return {
-        model: 'Member',
+        model: 'RankRecord',
         pageSize: 5,
         options: {
           can_create: false,
           can_edit: false,
         },
         filters: {
-          rank_type: 'rank_prize',
+          rank_type: 'send_diamond_amount',
+          duration: 'TOTAL',
         },
         cols: [
           {
             title: '用戶暱稱',
-            key: 'nickname',
+            key: 'author_nickname',
             filtering: {
-              search_field: 'kw_nickname',
+              search_field: 'kw_author__member__nickname',
             },
           },
           {
             title: '用戶賬號',
-            key: 'mobile',
+            key: 'author_mobile',
             filtering: {
-              search_field: 'kw_mobile',
+              search_field: 'kw_author__member__mobile',
             },
           },
           {
@@ -57,15 +64,15 @@
           },
           {
             title: '總收禮鑽石數',
-            key: 'debit_diamond',
+            key: 'receive_diamond_amount',
           },
           {
             title: '總送出鑽石數',
-            key: 'credit_diamond',
+            key: 'send_diamond_amount',
           },
           {
             title: '元氣指數',
-            key: 'star_index_receiver_balance',
+            key: 'star_index_amount',
           },
         ],
         actions: [],
@@ -76,3 +83,18 @@
   };
 </script>
 
+<style scoped lang="less" rel="stylesheet/less">
+  ul.duration-tabs {
+    display: inline-block;
+    margin-right: 20px;
+    li {
+      display: inline-block;
+      a {
+        color: black;
+        &.active {
+          color: blue;
+        }
+      }
+    }
+  }
+</style>
